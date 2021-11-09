6ec9df894ddab9613ecdbc1bb0f9cf1c355f45ee
Marc Zaku <mzaku@hotmail.de>
2021-11-09 10:32:21 +0100

    Removes using Keyword from main

--- Program.cs
+++ Program.cs
@@ -5,23 +5,23 @@ namespace SharpEngine
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            using (var window = new NativeWindow(800, 600, "MyWindowTitle"))
+        static void Main(string[] args) {
+            var window = new NativeWindow(800, 600, "MyWindowTitle");
+            
+            // Main application loop
+            while (!window.IsClosing)
             {
-                // Main application loop
-                while (!window.IsClosing)
-                {
-                    // OpenGL rendering
-                    // Implement any timing for flow control, etc (see Glfw.GetTime())
-                    
-                    // Swap the front/back buffers
-                    window.SwapBuffers();
-                    
-                    // Poll native operating system events (must be called or OS will think application is hanging)
-                    Glfw.PollEvents();
-                }
+                // OpenGL rendering
+                // Implement any timing for flow control, etc (see Glfw.GetTime())
+                
+                // Swap the front/back buffers
+                window.SwapBuffers();
+                
+                // Poll native operating system events (must be called or OS will think application is hanging)
+                Glfw.PollEvents();
             }
+            
+            window.Dispose();
         }
     }
 }
