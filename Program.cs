c80ad68f26020f809859fd4cf74372d85819b3fd
Marc Zaku <mzaku@hotmail.de>
2021-11-09 15:22:23 +0100

    Clear the Screen every Update

--- Program.cs
+++ Program.cs
@@ -26,6 +26,8 @@ namespace SharpEngine
             // engine rendering loop
             while (!Glfw.WindowShouldClose(window)) {
                 Glfw.PollEvents(); // react to window changes (position etc.)
+                glClearColor(.2f, .05f, .2f, 1);
+                glClear(GL_COLOR_BUFFER_BIT);
                 glDrawArrays(GL_TRIANGLES, 0, 3);
                 glFlush();
                 vertices[4] += 0.001f;
