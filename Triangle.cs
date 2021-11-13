21d0c22b99a646534d5ae82fb9681b7f1963d064
Marc Zaku <mzaku@hotmail.de>
2021-11-13 17:11:10 +0100

    Bind and Unbind Buffer Objects Correctly

--- Triangle.cs
+++ Triangle.cs
@@ -6,7 +6,9 @@ namespace SharpEngine {
 	public class Triangle {
             
 		Vertex[] vertices;
-            
+		uint vertexArray;
+		uint vertexBuffer;
+
 		public float CurrentScale { get; private set; }
             
 		public Triangle(Vertex[] vertices) {
@@ -15,15 +17,16 @@ namespace SharpEngine {
 			this.CurrentScale = 1f;
 		}
 		
-		static unsafe void LoadTriangleIntoBuffer() {
-			var vertexArray = glGenVertexArray();
-			var vertexBuffer = glGenBuffer();
+		 unsafe void LoadTriangleIntoBuffer() {
+			vertexArray = glGenVertexArray();
+			vertexBuffer = glGenBuffer();
 			glBindVertexArray(vertexArray);
 			glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 			glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.position)));
 			glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), Marshal.OffsetOf(typeof(Vertex), nameof(Vertex.color)));
 			glEnableVertexAttribArray(0);
 			glEnableVertexAttribArray(1);
+			glBindVertexArray(0);
 		}
 
 		public Vector GetMinBounds() {
@@ -69,10 +72,13 @@ namespace SharpEngine {
 		}
 
 		public unsafe void Render() {
+			glBindVertexArray(vertexArray);
+			glBindBuffer(GL_ARRAY_BUFFER, this.vertexBuffer);
 			fixed (Vertex* vertex = &this.vertices[0]) {
 				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this.vertices.Length, vertex, GL_DYNAMIC_DRAW);
 			}
 			glDrawArrays(GL_TRIANGLES, 0, this.vertices.Length);
+			glBindVertexArray(0);
 		}
 	}
 }
